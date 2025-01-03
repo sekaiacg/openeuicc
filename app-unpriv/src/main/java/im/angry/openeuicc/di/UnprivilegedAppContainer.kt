b90a147a913ee51a8e194a0797c6b1952d5e7f91
sekaiacg <sekaiacg@gmail.com>
2025-01-03 22:37:40 +0800

    9esim: Add new build variant and branding
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- app-unpriv/src/main/java/im/angry/openeuicc/di/UnprivilegedAppContainer.kt
+++ app-unpriv/src/main/java/im/angry/openeuicc/di/UnprivilegedAppContainer.kt
@@ -2,7 +2,7 @@ package im.angry.openeuicc.di
 
 import android.content.Context
 
-class UnprivilegedAppContainer(context: Context) : DefaultAppContainer(context) {
+open class UnprivilegedAppContainer(context: Context) : DefaultAppContainer(context) {
     override val uiComponentFactory by lazy {
         UnprivilegedUiComponentFactory()
     }
