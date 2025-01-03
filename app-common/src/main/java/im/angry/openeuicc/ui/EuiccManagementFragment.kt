24bac30361704b152719113f0fb062bff2229730
sekaiacg <sekaiacg@gmail.com>
2025-01-03 15:28:24 +0800

    ui: Extend the waiting time when switching profile to 50 seconds
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- app-common/src/main/java/im/angry/openeuicc/ui/EuiccManagementFragment.kt
+++ app-common/src/main/java/im/angry/openeuicc/ui/EuiccManagementFragment.kt
@@ -246,7 +246,7 @@ open class EuiccManagementFragment : Fragment(), EuiccProfilesChangedListener,
             val err = euiccChannelManagerService
                 .launchProfileSwitchTask(
                     slotId, portId, iccid, enable,
-                    reconnectTimeoutMillis = 30 * 1000
+                    reconnectTimeoutMillis = 50 * 1000
                 )
                 .waitDone()
 
