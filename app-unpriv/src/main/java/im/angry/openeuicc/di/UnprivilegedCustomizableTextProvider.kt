029c6be71d23dce52b7825e2120f4dcb05752bad
sekaiacg <sekaiacg@gmail.com>
2025-01-03 22:37:40 +0800

    9esim: Add new build variant and branding
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- app-unpriv/src/main/java/im/angry/openeuicc/di/UnprivilegedCustomizableTextProvider.kt
+++ app-unpriv/src/main/java/im/angry/openeuicc/di/UnprivilegedCustomizableTextProvider.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import im.angry.easyeuicc.R
 import im.angry.openeuicc.core.EuiccChannel
 
-class UnprivilegedCustomizableTextProvider(private val context: Context) :
+open class UnprivilegedCustomizableTextProvider(private val context: Context) :
     DefaultCustomizableTextProvider(context) {
     override fun formatNonUsbChannelName(logicalSlotId: Int): String =
         context.getString(R.string.channel_name_format_unpriv, logicalSlotId)
