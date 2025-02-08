295c97a3ee99f33e05b1ab8ecfebd7f2769e48f5
sekaiacg <sekaiacg@gmail.com>
2025-02-08 15:41:06 +0800

    ui: Report errors when handling notifications
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- libs/lpac-jni/src/main/java/net/typeblog/lpac_jni/impl/LocalProfileAssistantImpl.kt
+++ libs/lpac-jni/src/main/java/net/typeblog/lpac_jni/impl/LocalProfileAssistantImpl.kt
@@ -253,6 +253,7 @@ class LocalProfileAssistantImpl(
                 httpInterface.lastHttpException,
                 apduInterface.lastApduResponse,
                 apduInterface.lastApduException,
+                null,
             )
 
             // Cancel sessions if possible. This will overwrite recorded errors from HTTP and APDU interfaces.
@@ -267,10 +268,23 @@ class LocalProfileAssistantImpl(
         LpacJni.es10bDeleteNotification(contextHandle, seqNumber) == 0
 
     @Synchronized
-    override fun handleNotification(seqNumber: Long): Boolean =
-        LpacJni.handleNotification(contextHandle, seqNumber).also {
-            Log.d(TAG, "handleNotification $seqNumber = $it")
-        } == 0
+    override fun handleNotification(notification: LocalProfileNotification) {
+        val res = LpacJni.handleNotification(contextHandle, notification.seqNumber).also {
+            Log.d(TAG, "handleNotification ${notification.seqNumber} = $it")
+        }
+        if (res != 0) {
+            val err = LocalProfileAssistant.ProfileDownloadException(
+                lpaErrorReason = "Failed_To_Handle_Notification",
+                httpInterface.lastHttpResponse,
+                httpInterface.lastHttpException,
+                apduInterface.lastApduResponse,
+                apduInterface.lastApduException,
+                notification,
+            )
+            LpacJni.euiccHttpCleanup(contextHandle)
+            throw err
+        }
+    }
 
     @Synchronized
     override fun setNickname(iccid: String, nickname: String) {
