8f1576083fc3782971ea6bf748e50d180376d34c
sekaiacg <sekaiacg@gmail.com>
2025-03-18 13:28:05 +0800

    Vendors: Determine whether it is estkme when ATR exists
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- app-common/src/main/java/im/angry/openeuicc/util/Vendors.kt
+++ app-common/src/main/java/im/angry/openeuicc/util/Vendors.kt
@@ -37,7 +37,7 @@ private class EstkMe : EuiccVendor {
     private fun checkAtr(channel: EuiccChannel): Boolean {
         val iface = channel.apduInterface
         if (iface !is ApduInterfaceAtrProvider) return false
-        val atr = iface.atr ?: return false
+        val atr = iface.atr ?: return true
         for (index in atr.indices) {
             if (atr.size - index < PRODUCT_ATR_FPR.size) break
             if (atr.sliceArray(index until index + PRODUCT_ATR_FPR.size)
